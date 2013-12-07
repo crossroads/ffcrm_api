38fd615d969f8bab11972ca4b8fffe875eec0cf4
Brendan Neal <ahbrendan@gmail.com>
2013-12-07 13:05:37 +0800

    add password auth

--- app/controllers/ffcrm_api/v1/auth_controller.rb
+++ app/controllers/ffcrm_api/v1/auth_controller.rb
@@ -2,7 +2,13 @@ module FfcrmApi::V1
   class AuthController < FfcrmApi::V1::EntitiesController
     def get_single_access_token
       if params[:email] && user = User.find_by_email(params[:email])
-        render json: { :single_access_token => user.single_access_token }
+        password = "#{params[:password]}#{user.password_salt}"
+        if user.crypted_password == Authlogic::CryptoProviders::Sha512.encrypt(password)
+          render json: { :single_access_token => user.single_access_token }
+        else 
+          head 400
+          render :nothing => true
+        end
       else
         head 400
         render :nothing => true
