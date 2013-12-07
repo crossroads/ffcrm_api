5406bc6b184c966556505422e48680d9d833ac85
SoonKhen OwYong <dude@owyong.sk>
2013-12-07 07:09:52 +0000

    basic authentication working with single access token

--- app/controllers/ffcrm_api/application_controller.rb
+++ app/controllers/ffcrm_api/application_controller.rb
@@ -2,7 +2,6 @@ class FfcrmApi::ApplicationController < ActionController::Base
   def require_login
     unless params[:single_access_token] && User.find_by_single_access_token(params[:single_access_token])
       head 400
-      render :nothing => true
     end
   end
 end
