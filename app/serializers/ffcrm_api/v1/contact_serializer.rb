3ec01ac6a5936af4da672725c3d8d13f84a6c9de
Steve Kenworthy <steveyken@gmail.com>
2015-04-18 22:14:58 +0800

    Add contact fields

--- app/serializers/ffcrm_api/v1/contact_serializer.rb
+++ app/serializers/ffcrm_api/v1/contact_serializer.rb
@@ -2,7 +2,8 @@ module FfcrmApi::V1
 
   class ContactSerializer < ActiveModel::Serializer
     embed :ids, :include => true
-    attributes :id, :first_name, :last_name, :user_id, :opportunity_ids
+    attributes :id, :first_name, :last_name, :email, :alt_email,
+      :mobile, :facebook, :user_id, :opportunity_ids
     #~ has_many :opportunities
   end
 
